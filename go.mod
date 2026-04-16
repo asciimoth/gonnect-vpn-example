0ae9835e4ca24bcac44128a9ea9ab68a5400ea3c
AsciiMoth <ascii@moth.contact>
2026-04-16 13:07:35 +0400

    feat: add CopyWithLog helper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asciimoth/gonnect-vpn-example
 
 go 1.25.5
+
+require github.com/asciimoth/gonnect v0.4.0
---
+++ helpers/copy.go
@@ -0,0 +1,109 @@
+package helpers
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/asciimoth/gonnect-vpn-example/logger"
+	"github.com/asciimoth/gonnect/tun"
+)
+
+func CopyWithLog(
+	a, b tun.Tun,
+	aS, bS string,
+	offset int,
+	log logger.Logger,
+) error {
+	errCh := make(chan error, 2)
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		defer a.Close() // nolint
+		errCh <- copyOneWay(
+			a, b, offset, log,
+			fmt.Sprintf("%s --IP-> %s", a, b),
+		)
+	})
+	wg.Go(func() {
+		defer b.Close() // nolint
+		errCh <- copyOneWay(
+			b, a, offset, log,
+			fmt.Sprintf("%s <-IP-- %s", a, b),
+		)
+	})
+	wg.Wait()
+	close(errCh)
+	_ = a.Close()
+	_ = b.Close()
+	for err := range errCh {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func copyOneWay(
+	src, dst tun.Tun,
+	offset int,
+	log logger.Logger,
+	msg string,
+) error {
+	batchSize := src.BatchSize()
+	if dstBatch := dst.BatchSize(); dstBatch < batchSize {
+		batchSize = dstBatch
+	}
+	if batchSize <= 0 {
+		batchSize = 1
+	}
+
+	mtu, err := src.MTU()
+	if err != nil {
+		mtu = 1500
+	}
+	if dstMTU, err := dst.MTU(); err == nil && dstMTU > mtu {
+		mtu = dstMTU
+	}
+
+	// Allocate buffers with room for the offset
+	bufs := make([][]byte, batchSize)
+	sizes := make([]int, batchSize)
+	for i := range bufs {
+		bufs[i] = make([]byte, mtu+offset)
+	}
+
+	dataBufs := make([][]byte, batchSize)
+	writeBufs := make([][]byte, batchSize)
+	for i := range dataBufs {
+		dataBufs[i] = make([]byte, mtu+offset)
+	}
+
+	for {
+		n, err := src.Read(bufs, sizes, offset)
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			continue
+		}
+
+		for i := range n {
+			// Copy from read buffer (at offset) to write buffer (at offset)
+			copy(
+				dataBufs[i][offset:offset+sizes[i]],
+				bufs[i][offset:offset+sizes[i]],
+			)
+			// Slice to include the offset region so dst.Write can access data at offset
+			writeBufs[i] = dataBufs[i][:offset+sizes[i]]
+		}
+
+		log.Print(msg)
+		for written := 0; written < n; {
+			// Pass the full slice (including offset region) to dst.Write
+			wn, err := dst.Write(writeBufs[written:n], offset)
+			if err != nil {
+				return err
+			}
+			written += wn
+		}
+	}
+}
